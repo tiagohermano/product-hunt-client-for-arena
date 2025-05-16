bd447581df076ca93c8e70332b8c3915fb7fa202
Tiago Hermano <tiagofhermano@gmail.com>
2025-05-15 23:37:04 -0300

    chore: remove IntersectionObserver mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,41 +1,4 @@
-// Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
-// Mock IntersectionObserver
-class MockIntersectionObserver {
-  constructor(callback) {
-    this.callback = callback;
-    this.elements = new Set();
-    this.mockIsIntersecting = false;
-  }
-
-  observe(element) {
-    this.elements.add(element);
-  }
-
-  unobserve(element) {
-    this.elements.delete(element);
-  }
-
-  disconnect() {
-    this.elements.clear();
-  }
-
-  // Helper method to simulate an intersection
-  triggerIntersection(isIntersecting) {
-    this.mockIsIntersecting = isIntersecting;
-    this.callback(
-      [...this.elements].map((element) => ({
-        isIntersecting,
-        target: element,
-        intersectionRatio: isIntersecting ? 1 : 0,
-      })),
-      this,
-    );
-  }
-}
-
-global.IntersectionObserver = MockIntersectionObserver;
 
 // Mock next/navigation
 import { jest } from "@jest/globals";
